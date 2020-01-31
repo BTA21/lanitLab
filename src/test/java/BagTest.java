2de7a3e465c8474da2a07e5dc069b96386bdb1c3
NabievAzamat <azamat.nabiev21@yandex.ru>
2020-01-31 16:26:41 +0500

    exchanged bagtest

--- src/test/java/BagTest.java
+++ src/test/java/BagTest.java
@@ -1,7 +1,5 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class BagTest {
@@ -11,40 +9,59 @@ class BagTest {
     Item item2 = new Item("Игрушка1", 0.2d, 2,false);
     Item item3 = new Item("Мяч2", 0.5d, 4,false);
     Item item4 = new Item("Игрушка2", 0.2d, 2,false);
-    Item item5 = new Item("Книга1", 0.5d, 4,true);//  Плоский предмет
+    Item item5 = new Item("Книга1", 5d, 4,true);//  Плоский предмет
 
     @Test
     void putIn() {
-        bag.putIn(item1);
-        bag.putIn(item2);
-        bag.putIn(item3);
-        bag.putIn(item4);
-        bag.putIn(item5);
-        Double expected = 2d;
+        try{
+            bag.putIn(item1);
+            bag.putIn(item2);
+            bag.putIn(item3);
+            bag.putIn(item4);
+        } catch (ItemStoreExeption e) {
+            e.printStackTrace();
+        }
+        Double expected = 1.5d;
         Double actual = bag.weight;
         assertEquals(expected, actual);
     }
 
+//    @Test(expected=ItemStoreExeption.class)
+//    void putInException() {
+//        try{
+//            bag.putIn(item1);
+//            bag.putIn(item5);
+//        } catch (ItemStoreExeption e) {
+//            e.printStackTrace();
+//        }
+//    }
+
     @Test
     void pullOut() {
+        try{
         bag.putIn(item1);
         bag.putIn(item2);
         bag.putIn(item3);
         bag.putIn(item4);
-        bag.putIn(item5);
+        } catch (ItemStoreExeption e) {
+            e.printStackTrace();
+        }
         bag.pullOut("Игрушка1");
-        Double expected = 1.8d;
+        Double expected = 1.3d;
         Double actual = bag.weight;
         assertEquals(expected, actual);
     }
 
     @Test
     void testPullOut() {
+        try {
         bag.putIn(item1);
         bag.putIn(item2);
         bag.putIn(item3);
         bag.putIn(item4);
-        bag.putIn(item5);
+        } catch (ItemStoreExeption e) {
+            e.printStackTrace();
+        }
         bag.pullOut();
         Double expected = 1.5;
         Double expected1 = 1.8;
@@ -61,4 +78,5 @@ class BagTest {
         String actual =bag.toString();
         assertEquals(expected, actual);
     }
-}
+
+  }
